ac42543016a49eacc32a2e55fdc22cf338c84eca
Byron Anderson <byronanderson32@gmail.com>
2017-07-28 19:52:00 -0600

    new property: accurate

--- test/quiz_me_test.exs
+++ test/quiz_me_test.exs
@@ -6,23 +6,39 @@ defmodule QuizMeTest do
   use ExCheck
 
   test "is seeded random" do 
-  	seed = :os.timestamp()
-  	assert QuizMe.generate(seed) == QuizMe.generate(seed)
+    seed = :os.timestamp()
+    assert QuizMe.generate(seed) == QuizMe.generate(seed)
+  end
+
+  defp accurate?(quiz) do
+    case quiz do
+      {:x, n1, n2, n3} -> n1 * n2 == n3
+      {:%, n1, n2, n3} -> n1 / n2 == n3
+      {:+, n1, n2, n3} -> n1 + n2 == n3
+      {:-, n1, n2, n3} -> n1 - n2 == n3
+    end
+  end
+
+  property :accurate do
+    for_all random_seed in int() do
+      QuizMe.generate({random_seed, random_seed, random_seed})
+      |> accurate?()
+    end
   end
 
   property :all_numbers_integers do
-  	for_all random_seed in int() do
-  	  QuizMe.generate({random_seed, random_seed, random_seed})
-  	  |> QuizMe.numbers_involved()
-  	  |> Enum.all?(fn num -> is_integer(num) end)
-	end
+    for_all random_seed in int() do
+      QuizMe.generate({random_seed, random_seed, random_seed})
+      |> QuizMe.numbers_involved()
+      |> Enum.all?(fn num -> is_integer(num) end)
+    end
   end
 
   property :all_numbers_between_0_and_99 do
-  	for_all random_seed in int() do
-  	  QuizMe.generate({random_seed, random_seed, random_seed})
-  	  |> QuizMe.numbers_involved()
-  	  |> Enum.all?(fn num -> num < 100 && num >= 0 end)
-	end
+    for_all random_seed in int() do
+      QuizMe.generate({random_seed, random_seed, random_seed})
+      |> QuizMe.numbers_involved()
+      |> Enum.all?(fn num -> num < 100 && num >= 0 end)
+    end
   end
 end
