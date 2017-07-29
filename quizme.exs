5d568ac7338f5184b63ed545137f129e6012f008
Byron Anderson <byronanderson32@gmail.com>
2017-07-29 14:02:11 -0600

    add scoring

--- quizme.exs
+++ quizme.exs
@@ -1,17 +1,35 @@
 defmodule Runtime do
-  def loop() do
+  def loop({wins, losses} \\ {0, 0}) do
     {op, [n1, n2, n3], answer} = QuizMe.generate()
 
     IO.puts "#{n1} #{op} #{n2} = #{n3}"
 
+    case get_input() do
+      ^answer ->
+        IO.puts "Yep!"
+        wins = wins + 1
+      _ -> IO.puts "Nope, the answer was #{answer}"
+        losses = losses + 1
+    end
+    inspect_score({wins, losses})
+    loop({wins, losses})
+  end
+
+  defp get_input() do
     provided = IO.gets("> ") |> String.trim()
 
     case Integer.parse(provided) do
-      {^answer, ""} -> IO.puts "Yep!"
-      {_number, ""} -> IO.puts "Nope, the answer was #{answer}"
-      _ -> IO.puts "I don't recognize that response."
+      {number, ""} ->
+        number
+      _ ->
+        IO.puts "I don't recognize that response."
+        get_input()
     end
-    loop()
+  end
+
+  defp inspect_score({wins, losses}) do
+    IO.puts "Wins: #{wins}"
+    IO.puts "Losses: #{losses}"
   end
 end
 
