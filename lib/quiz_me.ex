c2170d4066c393e3e2f0d8f5d7532beb8c42acf2
Byron Anderson <byronanderson32@gmail.com>
2017-07-28 19:31:00 -0600

    multiplication operation returns integers

---
+++ lib/quiz_me.ex
@@ -0,0 +1,48 @@
+defmodule QuizMe do
+  @moduledoc """
+  Documentation for QuizMe.
+  """
+
+  @doc """
+  QuizMe generates questions of the form "5 x _ = 20" and determines whether the 
+  responses are correct.  Generation can be seeded for consistent responses
+
+  ## Examples
+
+      # iex> QuizMe.generate(1)
+      
+
+  """
+
+  @operations [:x]
+
+  def generate(seed \\ :os.timestamp) do
+    seed = :rand.seed(:exsplus, seed)
+    {op, seed} = random_operation(seed)
+    case op do
+      :x -> generate_multiplication_question(seed)
+    end
+  end
+
+  def numbers_involved({_op, n1, n2, n3}) do
+    [n1, n2, n3]
+  end
+
+  defp generate_multiplication_question(seed) do
+    {n1, seed} = random_nonnegative_integer(99, seed)
+    {n2, seed} = random_nonnegative_integer(99, seed)
+    result = n1 * n2
+    {:x, n1, n2, n1 * n2}
+  end
+
+  defp random_operation(seed) do
+    {index, seed} = :rand.uniform_s(Enum.count(@operations), seed)
+    op = Enum.at(@operations, index - 1)
+    {op, seed}
+  end
+
+  defp random_nonnegative_integer(max_val, seed) do
+    {val, seed} = :rand.uniform_s(max_val, seed)
+    {val - 1, seed}
+  end
+end
---
+++ test/quiz_me_test.exs
@@ -0,0 +1,26 @@
+defmodule QuizMeTest do
+  use ExUnit.Case
+  doctest QuizMe
+
+  use ExUnit.Case, async: false
+  use ExCheck
+
+  test "is seeded random" do 
+  	seed = :os.timestamp()
+  	assert QuizMe.generate(seed) == QuizMe.generate(seed)
+  end
+
+  property :all_numbers_integers do
+  	for_all random_seed in int() do
+  	  QuizMe.generate({random_seed, random_seed, random_seed})
+  	  |> QuizMe.numbers_involved()
+  	  |> Enum.all?(fn num -> is_integer(num) end)
+	end
+  end
+
+  # property :all_numbers_less_than_100 do
+  # end
+
+  # property :all_numbers_positive do
+  # end
+end
